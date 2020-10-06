28a1a57624269cbf1c729efd24e3aa2d2527a782
Marjone <marjonedev@gmail.com>
2020-10-06 15:53:06 +0800

    subscriber db migration

--- db/migrate/20201006074607_create_subscribers.rb
+++ db/migrate/20201006074607_create_subscribers.rb
@@ -1,6 +1,7 @@
 class CreateSubscribers < ActiveRecord::Migration[6.0]
   def change
     create_table :subscribers do |t|
+      t.integer :user_id, :index =>true, :null => false
       t.string :email
 
       t.timestamps
