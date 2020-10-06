578b91df132beab2c7fa64c45f6dac9c00674f5f
Ubuntu <ubuntu@dev-email-campaigner.us-central1-a.c.blissful-potion-253506.internal>
2020-10-06 12:26:08 +0000

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_173853) do
+ActiveRecord::Schema.define(version: 2020_10_06_074607) do
+
+  create_table "subscribers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_subscribers_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "username"
