0cee0859a5cd941c46e8ee3725cf076a7469e3dd
Marjone <marjonedev@gmail.com>
2020-10-06 22:16:37 +0800

    contacts

--- db/migrate/20201006122750_create_contacts.rb
+++ db/migrate/20201006122750_create_contacts.rb
@@ -1,6 +1,12 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
+      t.integer :user_id, :index =>true, :null => false
+      t.string :email, :null => false
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :phone
 
       t.timestamps
     end
